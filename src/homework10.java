871959c018b235f67d5f0efc45600e5fc3a07813
sinit <sinitsynajuliana@gmail.com>
2023-12-21 15:59:56 +1400

    gg

---
+++ src/homework10.java
@@ -0,0 +1,15 @@
+ public class homework10 {
+    public static void main(String[] args) {
+       // task 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println("Ф. И. О. сотрудника - " + fullName);
+    }
+}
+
+
+
